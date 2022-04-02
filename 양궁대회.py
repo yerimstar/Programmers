e159b322e1f77a9c23f68d378a5078e43afa93e0
yerimstar <yerimhuh@naver.com>
2022-04-02 18:26:47 +0900

    220402 - 2차

--- 양궁대회.py
+++ 양궁대회.py
@@ -1,18 +1,20 @@
-def ryan(n,info,value):
-    tmp = []
-    for i in range(len(info)):
-        if i < value:
-            tmp.append(0)
-            continue
+import itertools
 
-        if n > 0:
-            if n >= info[i] + 1:
-                tmp.append(info[i] + 1)
-                n -= (info[i] + 1)
+def ryan(n,ryan,appeach):
+    tmp = []
+    for i in range(11):
+        if ryan[i] == 1:
+            n -= (appeach[i] + 1)
+            tmp.append(appeach[i]+1)
+        else:
+            if i == 10 and n > 0:
+                tmp.append(n)
             else:
                 tmp.append(0)
-        else:
-            tmp.append(0)
+
+        if n < 0:
+            tmp = []
+            return tmp
     return tmp
 
 def arrow_sum(appeach,ryan):
@@ -21,6 +23,7 @@ def arrow_sum(appeach,ryan):
     for i in range(11):
         if appeach[i] == 0 and ryan[i] == 0:
             continue
+
         if appeach[i] >= ryan[i]:
             appeach_score += (10 - i)
         else:
@@ -30,22 +33,23 @@ def arrow_sum(appeach,ryan):
 
 def solution(n, info):
     answer = {}
+    choice = [1,0]
+    result = list(itertools.product(choice,repeat = 11)) # 중복순열
+    for r in result:
+        tmp = ryan(n,r,info)
+        if tmp:
+            appeach_score, ryan_score = arrow_sum(info, tmp)
+            answer[ryan_score-appeach_score] = tmp
 
-    for i in range(11):
-        tmp = ryan(n,info,i)
-        print(tmp)
-        appeach_score,ryan_score = arrow_sum(info,tmp)
-        print(ryan_score,appeach_score)
-        if tmp in answer.values():
-            answer = [-1]
-            return answer
-        answer[ryan_score-appeach_score] = tmp
+    answer = sorted(answer.items(), key=lambda x: x[0], reverse=True)[0]
+    if answer[0] < 0:
+        answer = [-1]
+    else:
+        answer = answer[1]
 
-    print(answer)
-    answer = sorted(answer.items() ,key=lambda x : x[0],reverse = True)[0][1]
     return answer
 
-n = 9
-info = [0,0,1,2,0,1,1,1,1,1,1]
+n = 10
+info = [0,0,0,0,0,0,0,0,3,4,3]
 answer = solution(n,info)
 print(answer)
