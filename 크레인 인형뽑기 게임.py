29d7ffc15a014fb52e59c59d04713c272ae407ca
yerimstar <yerimhuh@naver.com>
2022-06-25 23:34:17 +0900

    220625

--- 크레인 인형뽑기 게임.py
+++ 크레인 인형뽑기 게임.py
@@ -12,7 +12,8 @@ def solution(board, moves):
                         tmp.pop(-1)  # 기존에 있던 인형 없애기
                     else:  # 같은 인형이 아닌 경우 인형 추가
                         tmp.append(b[move - 1])
-                    break
+                b[move-1] = 0
+                break
     return answer
 
 board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
