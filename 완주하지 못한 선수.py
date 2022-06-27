c6171d190511c4bc961ca0a457d3804f19e87d29
yerimstar <yerimhuh@naver.com>
2022-06-27 21:36:35 +0900

    220627 - 수정중

--- 완주하지 못한 선수.py
+++ 완주하지 못한 선수.py
@@ -1,8 +1,16 @@
+'''
+시간복잡도 - O(N) for문 사용 -> 효율성 테스트 실패
 def solution(participant, completion):
     for c in completion:
         if c in completion:
             participant.remove(c)
     return participant[0]
+'''
+def solution(participant, completion):
+    if len(set(participant)) != len(participant):
+
+    else:
+        answer = list(set(participant)-set(completion))[0]
 
 participant = ["mislav", "stanko", "mislav", "ana"]
 completion = ["stanko", "ana", "mislav"]
