cd142d6e6137cdbe3d193b1211c3457cc83888c8
yerimstar <yerimhuh@naver.com>
2022-08-18 17:31:25 +0900

    [220818]

---
+++ BruteForce/소수찾기.py
@@ -0,0 +1,30 @@
+from itertools import permutations
+
+
+def is_primenumber(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+
+def solution(numbers):
+    numbers = list(numbers)
+    numbers_lst = []
+    # 최대 7 (7이하 문자열)
+    for i in range(1, len(numbers) + 1):
+        numbers_lst.extend(set(list(permutations(numbers, i))))
+
+    result = []
+    for number in numbers_lst:
+        num = int(''.join(number))
+        if num not in result and num >= 2 and is_primenumber(num):
+            result.append(num)
+
+    return len(result)
+
+
+'''
+만들 수 있는 모든 수의 조합 
+-> 소수 검사 
+'''
