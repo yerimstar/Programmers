6e1eb15521625f9e375518d3c3fbe277d5492665
yerimstar <yerimhuh@naver.com>
2022-08-18 17:20:25 +0900

    [220818]

--- BruteForce/모의고사.py
+++ BruteForce/모의고사.py
@@ -1,27 +1,22 @@
 def solution(answers):
-    math_1 = [1, 2, 3, 4, 5] * (len(answers) // 5 + 1)
-    math_2 = [2, 1, 2, 3, 2, 4, 2, 5] * (len(answers) // 8 + 1)
-    math_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * (len(answers) // 10 + 1)
+    math_1 = [1, 2, 3, 4, 5]
+    math_2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    math_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = [0,0,0]
     answer = []
-    ans_1 = 0
-    ans_2 = 0
-    ans_3 = 0
-    # N
-    for i in range(len(answers)):
-        if math_1[i] == answers[i]:
-            ans_1 += 1
-        if math_2[i] == answers[i]:
-            ans_2 += 1
-        if math_3[i] == answers[i]:
-            ans_3 += 1
-    result = max(ans_1, ans_2, ans_3)
 
-    if result == ans_1:
-        answer.append(1)
-    if result == ans_2:
-        answer.append(2)
-    if result == ans_3:
-        answer.append(3)
+    for idx,ans in enumerate(answers):
+        if ans == math_1[idx % 5]:
+            score[0] += 1
+        if ans == math_2[idx % 8]:
+            score[1] += 1
+        if ans == math_3[idx % 10]:
+            score[2] += 1
+
+    max_score = max(score)
+    for idx,sc in enumerate(score):
+        if sc == max_score:
+            answer.append(idx+1)
     return answer
 
 
