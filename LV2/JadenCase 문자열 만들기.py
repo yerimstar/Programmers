c94e405798ab1a894645260f89bd1b6365a6afbf
yerimstar <yerimhuh@naver.com>
2022-10-10 23:53:01 +0900

    [221010] 완료

--- LV2/JadenCase 문자열 만들기.py
+++ LV2/JadenCase 문자열 만들기.py
@@ -1,16 +1,23 @@
 def solution(s):
     # 공백 기준으로 단어 자르기
     words = s.split(' ')
-    print(words)
     answer = ''
-    for word in words:
-        if word == '':
+    for i in range(len(words)):
+        # 공백이 추가로 나오는 경우
+        if words[i] == '':
             answer += ' '
             continue
-        if word[0].isdigit():
-            answer += word[0]
+        # 공백 기준으로 잘랐기 때문에 다시 공백 추가
+        if not i == 0:
+            answer += ' '
+        # 첫 문자가 숫자라면 그대로
+        if words[i][0].isdigit():
+            answer += words[i][0]
+        # 첫 문자가 알파벳이라면 대문자 변환
         else:
-            answer += word[0].upper()
-        answer += word[1:].lower() + ' '
-    return answer.strip()
+            answer += words[i][0].upper()
+        # 나머지 문자는 소문자 처리
+        answer += words[i][1:].lower()
+    return answer
+
 solution("3people unFollowed me")
