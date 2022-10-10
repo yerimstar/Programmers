0b4372bed1f3821c01ed1d329cfde9b18cd6c1d5
yerimstar <yerimhuh@naver.com>
2022-10-10 23:47:40 +0900

    [221010] 테스트 8 실패

--- LV2/JadenCase 문자열 만들기.py
+++ LV2/JadenCase 문자열 만들기.py
@@ -1,11 +1,16 @@
 def solution(s):
     # 공백 기준으로 단어 자르기
     words = s.split(' ')
+    print(words)
     answer = ''
     for word in words:
-        if word[0].isalpha():
-            answer += word[0].upper()
-        else:
+        if word == '':
+            answer += ' '
+            continue
+        if word[0].isdigit():
             answer += word[0]
+        else:
+            answer += word[0].upper()
         answer += word[1:].lower() + ' '
+    return answer.strip()
 solution("3people unFollowed me")
